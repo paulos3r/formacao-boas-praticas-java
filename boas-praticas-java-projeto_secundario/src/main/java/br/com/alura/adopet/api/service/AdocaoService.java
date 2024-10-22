4b1f976b813906c622d0bcdc98a9535da452d7e2
Paulo Sergio <036paulo@gmail.com>
2024-10-21 21:10:55 -0300

    Foi ajustado a classe Adocao com construtor para ajudar a otimizar o codigo

--- boas-praticas-java-projeto_secundario/src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
+++ boas-praticas-java-projeto_secundario/src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
@@ -3,10 +3,8 @@ package br.com.alura.adopet.api.service;
 import br.com.alura.adopet.api.dto.AprovacaoAdocaoDto;
 import br.com.alura.adopet.api.dto.ReprovacaoAdocaoDto;
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
-import br.com.alura.adopet.api.exception.ValidacaoException;
 import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.Pet;
-import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
 import br.com.alura.adopet.api.repository.PetRepository;
@@ -15,7 +13,6 @@ import br.com.alura.adopet.api.validacoes.IValidacaoSolicitacaoAdocao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -43,15 +40,10 @@ public class AdocaoService {
 
     validacao.forEach(v->v.validar(dto));
 
-    Adocao adocao = new Adocao();
-
-    adocao.setData(LocalDateTime.now());
-    adocao.setStatus(StatusAdocao.AGUARDANDO_AVALIACAO);
-    adocao.setPet(pet);
-    adocao.setTutor(tutor);
-    adocao.setMotivo(dto.motivo());
+    Adocao adocao = new Adocao(tutor,pet, dto.motivo());
 
     repository.save(adocao);
+
     String mensagem = "Olá " +adocao.getPet().getAbrigo().getNome() +"!\n\nUma solicitação de adoção foi registrada hoje para o pet: " +adocao.getPet().getNome() +". \nFavor avaliar para aprovação ou reprovação.";
     emailService.enviarEmail(adocao.getPet().getAbrigo().getEmail(),"Solicitação de adoção",mensagem);
   }
@@ -59,8 +51,8 @@ public class AdocaoService {
   public void aprovar(AprovacaoAdocaoDto dto){
     Adocao adocao = repository.getReferenceById(dto.idAdocao());
 
-    adocao.setStatus(StatusAdocao.APROVADO);
-    // repository.save(adocao);
+    adocao.marcarComoAprovado();
+
     String mensagem = "Parabéns " +adocao.getTutor().getNome() +"!\n\nSua adoção do pet " +adocao.getPet().getNome() +", solicitada em " +adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) +", foi aprovada.\nFavor entrar em contato com o abrigo " +adocao.getPet().getAbrigo().getNome() +" para agendar a busca do seu pet.";
     emailService.enviarEmail(adocao.getPet().getAbrigo().getEmail(),"Adoção aprovada",mensagem );
 
@@ -68,10 +60,7 @@ public class AdocaoService {
   public void reprovar(ReprovacaoAdocaoDto dto){
     Adocao adocao = repository.getReferenceById(dto.idAdocao());
 
-    adocao.setStatus(StatusAdocao.REPROVADO);
-    adocao.setJustificativaStatus(dto.justificativa());
-
-    // repository.save(adocao);  jpa faz update automatico
+    adocao.marcarComoReprovado(dto.justificativa());
 
     String mensagem = "Olá " +adocao.getTutor().getNome() +"!\n\nInfelizmente sua adoção do pet " +adocao.getPet().getNome() +", solicitada em " +adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) +", foi reprovada pelo abrigo " +adocao.getPet().getAbrigo().getNome() +" com a seguinte justificativa: " +adocao.getJustificativaStatus();
     emailService.enviarEmail(adocao.getPet().getAbrigo().getEmail(),"Adoção reprovada", mensagem );
